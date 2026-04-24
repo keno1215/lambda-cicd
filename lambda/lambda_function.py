049a6b2d94629972f075dd233a03418fdb0296df
keno1215 <kenoconnell73@gmail.com>
2026-04-24 18:49:07 -0400

    modified lambda function name in file

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello github actions workflow vscode')
+        'body': json.dumps('Hello actions workflow vscode')
     }
